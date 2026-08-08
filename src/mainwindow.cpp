69f98e329b7c1bbdb32bd53a34d8020dca490fa5
C0m3b4ck <push.decker0k@icloud.com>
2026-08-08 13:42:26 +0200

    Qt: added login and register workspaces

---
+++ src/mainwindow.cpp
@@ -0,0 +1,108 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+// =============== MAIN WINDOW ===================
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// ============== CLEAR BUTTONS ==================
+void MainWindow::on_btnClear_username_register_clicked()
+{
+    ui->txtUsr_register->setText("");
+}
+
+
+void MainWindow::on_btnClear_password1_register_clicked()
+{
+    ui->txtPwd1_register->setText("");
+}
+
+
+void MainWindow::on_btnClear_password2_register_clicked()
+{
+    ui->txtPwd2_register->setText("");
+}
+
+// =============== HELP BUTTONS =======================
+void MainWindow::on_btnHelp_pwdStrenght_register_clicked()
+{
+    QMessageBox::information(this, tr("Password Strength"),
+    tr("Judges password strength based on:  \n * length, \n * characters used (numbers, letters, special), \n * capitalization, \n * whether the password is a set of words. \n The higher the password strength, the harder it is to guess or crack using specialized tools."));
+}
+
+
+void MainWindow::on_btnHelp_role_register_clicked()
+{
+    QMessageBox::information(this, tr("User Roles"), tr("1. User \n Has the rights to manage books, readers and loans. Cannot access database and advanced settings. \n 2. Admin \n Has access to everything that User has access to plus database-related functions. \n 3. Superadmin \n The highest role, has access to all of the functions that Admin has and some critical functions, including salt modification and updating the app."));
+}
+
+void MainWindow::on_btnHelp_role_login_clicked()
+{
+    QMessageBox::information(this, tr("User Roles"), tr("1. User \n Has the rights to manage books, readers and loans. Cannot access database and advanced settings. \n 2. Admin \n Has access to everything that User has access to plus database-related functions. \n 3. Superadmin \n The highest role, has access to all of the functions that Admin has and some critical functions, including salt modification and updating the app."));
+}
+
+////////////////////////////// MENU BUTTONS //////////////////////////////////////////////
+void MainWindow::on_actionLog_out_triggered()
+{
+    // change logged in bool to false or whatever the checking function will be
+    // set role to null, switch workspace to login
+}
+
+void MainWindow::on_actionLog_in_triggered()
+{
+    // navigate to login workspace (magic numbers)
+    ui->workspaces->setCurrentIndex(1);
+}
+
+void MainWindow::on_actionRegister_triggered()
+{
+    // navigate to register workspace (magic numbers)
+    ui->workspaces->setCurrentIndex(0);
+}
+
+////////////////////////////// FUNCTIONAL BUTTONS /////////////////////////////////////////
+// =============== ACCOUT MANAGEMENT ==================
+void MainWindow::on_btnRegister_clicked()
+{
+    // check whether username and password are the same
+    // check if both passwords match
+    // check password score - if it is low, warn user
+    // if both pass, create DB if not exist, make hash from usr+pwd+salt, write to DB
+    // then let user in with the role that they registered with (bypass login)
+}
+
+void MainWindow::on_actionClose_Application_triggered()
+{
+    QMessageBox box(this);
+    box.setIcon(QMessageBox::Warning);
+    box.setWindowTitle(tr("Close Application"));
+    box.setText(tr("Close application?"));
+    box.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    box.setDefaultButton(QMessageBox::No);
+
+    if (box.exec() == QMessageBox::Yes) {
+        // gracefully close all streams etc etc
+        QCoreApplication::exit();
+    }
+}
+
+
+void MainWindow::on_btnLogin_clicked()
+{
+    // check if empty usr/pwd
+    // if not - make hash from usr + pwd + salt
+    // then search for it in user DB using SQL
+    // if EOF - no such account, refuse
+    // if not EOF - log in
+}
+
---
+++ src/mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    void on_btnClear_username_register_clicked();
+
+    void on_btnClear_password1_register_clicked();
+
+    void on_btnClear_password2_register_clicked();
+
+    void on_btnHelp_pwdStrenght_register_clicked();
+
+    void on_btnHelp_role_register_clicked();
+
+    void on_btnRegister_clicked();
+
+    void on_actionClose_Application_triggered();
+
+    void on_btnLogin_clicked();
+
+    void on_btnHelp_role_login_clicked();
+
+    void on_actionLog_out_triggered();
+
+    void on_actionLog_in_triggered();
+
+    void on_actionRegister_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
